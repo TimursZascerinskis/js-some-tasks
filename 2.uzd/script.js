b98fc0b8e090b54ffcec2e864e035647b72437a8
unknown <your@email.example>
2025-11-07 11:24:51 +0200

    ToDoList - push method

---
+++ 2.uzd/script.js
@@ -0,0 +1,14 @@
+ const radiusInput = document.getElementById("text");
+        const saveButton = document.getElementById("saveButton");
+        const sarakstsList = document.getElementById("saraksts");
+
+
+        function addToHistory() {
+            const text = String(radiusInput.value);
+
+            const historyItem = document.createElement("li");
+            historyItem.textContent = `${text}  `;
+            sarakstsList.appendChild(historyItem);
+        }
+
+        saveButton.addEventListener("click", addToHistory);
